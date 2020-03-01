9a9283e323520897c99c2c3fb3b66c6611497bd9
Emel Gumustop <emelgumustop@hotmail.com>
2020-03-01 13:56:06 -0500

    get.url opean an url

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -7,14 +7,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicNavigation {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // to start selenium script we need:
         //setup webdriver (browser driver) and create webdriver object
         WebDriverManager.chromedriver().setup();
 
        WebDriver driver=new ChromeDriver();
-
         //in selenium everything starts from web driver interface
-             driver.get("http://facebook.com");
+             driver.get("http://google.com");
+        Thread.sleep (3000);//for demo, wait 3 seconds
+
+        String title =driver.getTitle(); //returns <<title>Google</title>
+        String expectedTitle= "yahoo";
+
+        System.out.println("title is " + title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+        driver.close();
+
+
     }
 }
