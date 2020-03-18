1adc0448d96116c2f5b82be892b1d9033757cd22
emelgmstp <emelgumustop@hotmail.com>
2020-03-18 19:34:18 -0400

    Action Test

--- src/test/java/com/automation/tests/day9/ActionsTests.java
+++ src/test/java/com/automation/tests/day9/ActionsTests.java
@@ -2,7 +2,10 @@ package com.automation.tests.day9;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,16 +13,23 @@ import org.testng.annotations.Test;
 public class ActionsTests {
 
     private WebDriver driver;
+    private Actions actions;
+
 
     @BeforeMethod
     public void setup(){
         driver= DriverFactory.createDriver("chrome");
+        actions=new Actions(driver);
 
     }
     @Test
     public void hoverOnImage(){
         driver.get("http://practice.cybertekschool.com/hovers");
         BrowserUtils.wait(2);
+        WebElement img1=driver.findElement(By.xpath("(//img)[1]"));
+        WebElement img2=driver.findElement(By.xpath("(//img)[2]"));
+
+        actions.moveToElement(img1).pause(1000).moveToElement(img2).build().perform();
 
     }
 
@@ -30,6 +40,7 @@ public class ActionsTests {
 
     @AfterMethod
     public void teardown(){
+        BrowserUtils.wait(2);
         driver.quit();
     }
 
