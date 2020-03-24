c3f9d7aa1c19d7ab1e76538b8cb8fd0a704bb370
emelgmstp <emelgumustop@hotmail.com>
2020-03-24 19:42:35 -0400

    configuration.properties

---
+++ src/test/java/com/automation/tests/day13/SystemProperties.java
@@ -0,0 +1,29 @@
+package com.automation.tests.day13;
+
+import org.testng.annotations.Test;
+
+public class SystemProperties {
+
+
+
+
+
+    @Test
+    public void test(){
+        //        System.out.println( System.getProperty("user.dir")) + "/pom.xml"
+        System.out.println( System.getProperty("user.dir"));
+        System.out.println( System.getProperty("os.name"));
+        System.out.println( System.getProperty("user.name"));
+        System.out.println( System.getProperty("java.home"));
+        System.out.println( System.getProperty("java.version"));
+
+        System.out.println( System.getProperty("os.arch"));
+        System.out.println( System.getProperty("user.home"));
+        String pathToDownloads=System.getProperty("user.home") + "/Downloads";
+        System.out.println("pathToDownloads = " + pathToDownloads);
+
+        System.out.println( System.getProperty("os.version"));
+
+
+    }
+}
